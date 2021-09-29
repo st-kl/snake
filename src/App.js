7bc7ebd59244b787a733b42c10f933ae058413ce
Steffen Kludt <steffen.kludt@thinkmoney.co.uk>
2021-09-29 16:37:20 +0100

    add collision with self logic

--- src/App.js
+++ src/App.js
@@ -1,54 +1,53 @@
 import { useEffect, useState } from 'react';
-import './App.css';
 
 function App() {
   const rows = 15;
   const columns = 21;
 
   const createRandomCoordinates = () => {
-    let isFree = false;
+    let isAvailable = false;
 
-    while (!isFree) {
-      const row = Math.floor(Math.random() * rows) + 1;
-      const col = Math.floor(Math.random() * columns) + 1;
+    while (!isAvailable) {
+      const x = Math.floor(Math.random() * rows);
+      const y = Math.floor(Math.random() * columns);
 
-      if (
-        !snake.filter((part) => part.row === row && part.col === col).length
-      ) {
-        isFree = true;
-        return { row: row, col: col };
+      if (!snake.filter((part) => part.x === x && part.y === y).length) {
+        isAvailable = true;
+        return { x, y };
       }
     }
   };
 
   const [grid, setGrid] = useState([]);
   const [direction, setDirection] = useState('down');
-  const [snake, setSnake] = useState([{ row: 4, col: 11 }]);
+  const [snake, setSnake] = useState([
+    { x: Math.floor(rows / 2), y: Math.floor(columns / 2) },
+  ]);
   const [food, setFood] = useState(createRandomCoordinates());
   const [gameOver, setGameOver] = useState(true);
   const [points, setPoints] = useState(0);
   const [buttonText, setButtonText] = useState('Play');
 
-  const updateGrid = () => {
+  const createGrid = () => {
     setGrid(() => {
       const grid = [];
-      for (let i = 1; i <= rows; i++) {
-        for (let j = 1; j <= columns; j++) {
+      for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
           let isSnake = false;
 
           for (const part of snake) {
-            if (part.row === i && part.col === j) {
+            if (part.x === i && part.y === j) {
               isSnake = true;
             }
           }
 
           if (isSnake) {
-            grid.push({ row: i, col: j, class: 'grid-item snake' });
-          } else if (food.row === i && food.col === j) {
-            grid.push({ row: i, col: j, class: 'grid-item food' });
+            grid.push({ x: i, y: j, class: 'grid-item snake' });
+          } else if (food.x === i && food.y === j) {
+            grid.push({ x: i, y: j, class: 'grid-item food' });
             isSnake = false;
           } else {
-            grid.push({ row: i, col: j, class: 'grid-item' });
+            grid.push({ x: i, y: j, class: 'grid-item' });
           }
         }
       }
@@ -62,16 +61,16 @@ function App() {
       let head = snake.slice(0)[0];
       switch (direction) {
         case 'up':
-          head = { row: head.row - 1, col: head.col };
+          head = { x: head.x - 1, y: head.y };
           break;
         case 'down':
-          head = { row: head.row + 1, col: head.col };
+          head = { x: head.x + 1, y: head.y };
           break;
         case 'left':
-          head = { row: head.row, col: head.col - 1 };
+          head = { x: head.x, y: head.y - 1 };
           break;
         case 'right':
-          head = { row: head.row, col: head.col + 1 };
+          head = { x: head.x, y: head.y + 1 };
           break;
         default:
           break;
@@ -80,67 +79,32 @@ function App() {
     }
   };
 
-  useEffect(() => {
-    document.onkeydown = onKeyDown;
-    checkCollision();
-    eatFood();
-
-    const runGame = setInterval(() => {
-      moveSnake();
-    }, 300);
-    return () => clearInterval(runGame);
-  });
-
-  function onKeyDown(e) {
-    switch (e.keyCode) {
-      case 38:
-        if (direction !== 'down') setDirection('up');
-        break;
-      case 40:
-        if (direction !== 'up') setDirection('down');
-
-        break;
-      case 37:
-        if (direction !== 'right') setDirection('left');
-
-        break;
-      case 39:
-        if (direction !== 'left') setDirection('right');
-
-        break;
-    }
-  }
-
   const growSnake = () => {
     setSnake([...snake, snake.slice(-1)[0]]);
   };
 
   const eatFood = () => {
-    if (snake[0].row === food.row && snake[0].col === food.col) {
+    if (snake[0].x === food.x && snake[0].y === food.y) {
       setFood(createRandomCoordinates());
       growSnake();
       setPoints(points + 1);
     }
   };
 
-  useEffect(() => {
-    updateGrid();
-  }, [snake, food]);
-
   const checkCollision = () => {
     let isSnake = false;
 
-    for (const part of snake.slice(3)) {
-      if (part.row === snake[0].row && part.col === snake[0].col) {
+    for (const part of snake.slice(4)) {
+      if (part.x === snake[0].x && part.y === snake[0].y) {
         isSnake = true;
       }
     }
 
     if (
-      snake[0].row === 0 ||
-      snake[0].row === rows + 1 ||
-      snake[0].col === 0 ||
-      snake[0].col === columns + 1 ||
+      snake[0].x === -1 ||
+      snake[0].x === rows ||
+      snake[0].y === -1 ||
+      snake[0].y === columns ||
       isSnake
     ) {
       setGameOver(true);
@@ -148,18 +112,51 @@ function App() {
     }
   };
 
+  function keyPress(event) {
+    switch (event.keyCode) {
+      case 38:
+        if (direction !== 'down') setDirection('up');
+        break;
+      case 40:
+        if (direction !== 'up') setDirection('down');
+        break;
+      case 37:
+        if (direction !== 'right') setDirection('left');
+        break;
+      case 39:
+        if (direction !== 'left') setDirection('right');
+        break;
+    }
+  }
+
+  // effects
+  useEffect(() => {
+    document.onkeydown = keyPress;
+    checkCollision();
+    eatFood();
+
+    const runGame = setInterval(() => {
+      moveSnake();
+    }, 200);
+    return () => clearInterval(runGame);
+  });
+
+  useEffect(() => {
+    createGrid();
+  }, [snake, food]);
+
   return (
-    <div className='App'>
-      <div className='content'>
+    <div className="App">
+      <div className="content">
         <h1>Snake</h1>
         {!gameOver ? (
           <div>
-            <div className='grid'>
+            <div className="grid">
               {grid.map((cell) => {
                 return (
                   <div
                     className={`${cell.class}`}
-                    key={`grid-item-${cell.row}-${cell.col}`}
+                    key={`grid-item-${cell.x}-${cell.y}`}
                   ></div>
                 );
               })}
@@ -167,12 +164,17 @@ function App() {
             <p>Points: {points}</p>
           </div>
         ) : (
-          <div className='info'>
+          <div className="info">
             <p>Points: {points}</p>
             <button
               onClick={() => {
                 setGameOver(false);
-                setSnake([{ row: 4, col: 11 }]);
+                setSnake([
+                  {
+                    x: Math.floor(rows / 2),
+                    y: Math.floor(columns / 2),
+                  },
+                ]);
                 setFood(createRandomCoordinates());
                 setPoints(0);
                 setDirection('down');
